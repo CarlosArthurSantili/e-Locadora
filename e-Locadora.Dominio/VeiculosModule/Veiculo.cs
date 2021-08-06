dae87722102c61e9848437f75014a580a7c0cdee
odair.tiodaia95@gmail.com <odair.tiodaia95@gmail.com>
2021-08-06 10:40:22 -0300

    Clase Veiculo feita mais classes enusn pertencentes

---
+++ e-Locadora.Dominio/VeiculosModule/Veiculo.cs
@@ -0,0 +1,41 @@
+﻿using e_Locadora.Dominio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora.Dominio.VeiculosModule
+{
+    public class Veiculo : EntidadeBase
+    {
+        public Veiculo(string placa,string fabricante,string qtdlitros,string chassi,string cor,
+            int passageiros,int ano,int portamalas, CombustivelEnum combustivel)
+        {
+            Placa = placa;
+            Fabricante = fabricante;
+            QtdLitrosTanque = qtdlitros;
+            NumeroChassi = chassi;
+            Cor = cor;
+            CapacidadeOcupantes = passageiros;
+            AnoFabricacao = ano;
+            TamanhoPortaMalas = portamalas;
+            Combustivel = new Combustivel(combustivel);
+        }
+
+        public string Placa { get; }
+        public string Fabricante { get; }
+        public string QtdLitrosTanque { get; }
+        public string NumeroChassi { get; }
+        public string Cor { get; }
+        public int CapacidadeOcupantes { get; }
+        public int AnoFabricacao { get; }
+        public int TamanhoPortaMalas { get; }
+        public Combustivel Combustivel { get; }
+
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
