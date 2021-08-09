0c9cf5d48e20a93471994ffc99fbe32dc9d55cf7
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-09 17:11:36 -0300

    Iserindo Tela Principal e seus componentes

---
+++ e-Locadora.WindowsApp/Shared/ConfiguracaoToolTips.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAgenda.WindowsApp.Shared
+{
+    public class ConfiguracaoToolTips
+    {
+        public ConfiguracaoToolTips()
+        {
+            Adicionar = "Adicionar";
+            Editar = "Editar";
+            Excluir = "Excluir";
+            Agrupar = "Agrupar";
+            Desagrupar = "Desagrupar";
+            Filtrar = "Filtrar";
+        }
+
+        public string Adicionar { get; internal set; }
+        public string Editar { get; internal set; }
+        public string Excluir { get; internal set; }
+        public string Agrupar { get; internal set; }
+        public string Desagrupar { get; internal set; }
+        public string Filtrar { get; internal set; }
+    }
+}
