4ddb6159161a280016948afe61a9db28aa169c02
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-08-09 13:27:57 -0300

    Criado a classe GrupoVeiculoControlador e implementado seus comandos SQL

---
+++ e-Locadora.Controladores/VeiculoModule/GrupoVeiculoControlador.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora.Controladores.VeiculoModule
+{
+    public class GrupoVeiculoControlador
+    {
+        private const string sqlInserirGrupoVeiculo =
+            @"INSERT INTO TBCATEGORIAS 
+	                (
+		                [CATEGORIA], 
+		                [VALORDIARIOKM], 
+		                [VALORDIARIO],
+                        [VALORCONTROLADODIARIOKM], 
+		                [VALORCONTROLADORDIARIO],
+                        [VALORLIVRE]
+	                ) 
+	                VALUES
+	                (
+                        @CATEGORIA, 
+		                @VALORDIARIOKM, 
+		                @VALORDIARIO,
+                        @VALORCONTROLADODIARIOKM, 
+		                @VALORCONTROLADORDIARIO,
+                        @VALORLIVRE
+	                )";
+
+        private const string sqlEditarGrupoVeiculo =
+            @"UPDATE TBCATEGORIAS
+                    SET
+                        [CATEGORIA], 
+		                [VALORDIARIOKM], 
+		                [VALORDIARIO],
+                        [VALORCONTROLADODIARIOKM], 
+		                [VALORCONTROLADORDIARIO],
+                        [VALORLIVRE]
+                    WHERE 
+                        ID = @ID";
+
+        private const string sqlExcluirGrupoVeiculo =
+            @"DELETE 
+	                FROM
+                        TBCATEGORIAS
+                    WHERE 
+                        ID = @ID";
+
+        private const string sqlSelecionarGrupoVeiculoPorId =
+            @"SELECT
+                        [CATEGORIA], 
+		                [VALORDIARIOKM], 
+		                [VALORDIARIO],
+                        [VALORCONTROLADODIARIOKM], 
+		                [VALORCONTROLADORDIARIO],
+                        [VALORLIVRE]
+	                FROM
+                        TBCATEGORIAS
+                    WHERE 
+                        ID = @ID";
+
+        private const string sqlSelecionarTodosGrupoVeiculos =
+            @"SELECT
+                        [CATEGORIA], 
+		                [VALORDIARIOKM], 
+		                [VALORDIARIO],
+                        [VALORCONTROLADODIARIOKM], 
+		                [VALORCONTROLADORDIARIO],
+                        [VALORLIVRE]
+	                FROM
+                        TBCATEGORIAS ORDER BY CATEGORIA;";
+
+        private const string sqlExisteGrupoVeiculo =
+            @"SELECT 
+                COUNT(*) 
+            FROM 
+                [TBCATEGORIAS]
+            WHERE 
+                [ID] = @ID";
+    }
+}
