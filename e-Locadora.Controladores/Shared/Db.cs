e3cb70eed634bf8ae8caaffa891988aa4a4ec933
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-09 09:16:43 -0300

    Instalando Pacote nuget para classe DB

---
+++ e-Locadora.Controladores/Shared/Db.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora.Controladores
+{
+    public delegate T ConverterDelegate<T>(IDataReader reader);
+    public static class Db
+    {
+        private static readonly string bancoDeDados;
+        private static readonly string connectionString = "";
+        private static readonly string nomeProvider;
+        private static readonly DbProviderFactory fabricaProvedor;
+
+        static Db()
+        {
+            bancoDeDados = ConfigurationManager.AppSettings["bancoDeDados"];
+
+            connectionString = ConfigurationManager.ConnectionStrings[bancoDeDados].ConnectionString;
+
+            nomeProvider = ConfigurationManager.ConnectionStrings[bancoDeDados].ProviderName;
+
+            fabricaProvedor = DbProviderFactories.GetFactory(nomeProvider);
+        }
+
+    }
+}
