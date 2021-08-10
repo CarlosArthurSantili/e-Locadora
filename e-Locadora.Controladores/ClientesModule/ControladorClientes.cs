7cfe20de7954171321aaf839df16b066b3c24ee1
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-10 15:45:07 -0300

    Criado Classe Controlador Cliente, erdando de Controlador Generica

---
+++ e-Locadora.Controladores/ClientesModule/ControladorClientes.cs
@@ -0,0 +1,42 @@
+﻿using e_Locadora.Controladores.Shared;
+using e_Locadora.Dominio.ClientesModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora.Controladores.ClientesModule
+{
+    public class ControladorClientes : Controlador<Clientes>
+    {
+        public override string InserirNovo(Clientes registro)
+        {
+            throw new NotImplementedException();
+        }
+        public override string Editar(int id, Clientes registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Clientes SelecionarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Clientes> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
