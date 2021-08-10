a38c5ad6a490925e390dc23f016525821bc5b2b7
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-10 17:02:10 -0300

    Adicionado Telas , implementado a Configuração de Tool Box de clientes

---
+++ e-Locadora.WindowsApp/ClientesModule/ConfiguracaoClientesToolBox.cs
@@ -0,0 +1,44 @@
+﻿using e_Locadora.WindowsApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora.WindowsApp.ClientesModule
+{
+    public class ConfiguracaoClientesToolBox : IConfiguracaoToolBox
+    {
+        public string TipoCadastro 
+        {
+            get { return "Cadastro de Clientes"; }
+        }
+
+        public string ObterDescricao()
+        {
+            return TipoCadastro;
+        }
+
+        public ConfiguracaoEstadoBotoes ObterEstadoBotoes()
+        {
+            return new ConfiguracaoEstadoBotoes()
+            {
+                Agrupar = true,
+                Desagrupar = true,
+                Filtrar = false
+            };
+        }
+
+        public ConfiguracaoToolTips ObterToolTips()
+        {
+            return new ConfiguracaoToolTips()
+            {
+                Adicionar = "Adicionar um novo Cliente",
+                Editar = "Atualizar um Cliente existente",
+                Excluir = "Excluir um Cliente existente",
+                Agrupar = "Agrupar Clientes",
+                Desagrupar = "Desagrupar Clientes"
+            };
+        }
+    }
+}
