36e9f330ee41060739f8847927688af8012edbb4
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-10 16:05:36 -0300

    Todos os metodos da clase controlador Clientes implementados

--- e-Locadora.Controladores/ClientesModule/ControladorClientes.cs
+++ e-Locadora.Controladores/ClientesModule/ControladorClientes.cs
@@ -2,6 +2,7 @@
 using e_Locadora.Dominio.ClientesModule;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace e_Locadora.Controladores.ClientesModule
 {
     public class ControladorClientes : Controlador<Clientes>
     {
-        #region
+        #region Queris
         private const string sqlInserirCliente =
             @"INSERT INTO TBCLIENTES 
 	                (
@@ -85,31 +86,88 @@ namespace e_Locadora.Controladores.ClientesModule
         #endregion
         public override string InserirNovo(Clientes registro)
         {
-            throw new NotImplementedException();
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                registro.Id = Db.Insert(sqlInserirCliente, ObtemParametrosClientes(registro));
+            }
+
+            return resultadoValidacao;
         }
+
+
         public override string Editar(int id, Clientes registro)
         {
-            throw new NotImplementedException();
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                registro.Id = id;
+                Db.Update(sqlEditarCliente, ObtemParametrosClientes(registro));
+            }
+
+            return resultadoValidacao;
         }
 
         public override bool Excluir(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Db.Delete(sqlExcluirCliente, AdicionarParametro("ID", id));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public override bool Existe(int id)
         {
-            throw new NotImplementedException();
+            return Db.Exists(sqlExisteCliente, AdicionarParametro("ID", id));
         }
 
         public override Clientes SelecionarPorId(int id)
         {
-            throw new NotImplementedException();
+            return Db.Get(sqlSelecionarClientePorId, ConverterEmCliente, AdicionarParametro("ID", id));
         }
 
         public override List<Clientes> SelecionarTodos()
         {
-            throw new NotImplementedException();
+            return Db.GetAll(sqlSelecionarTodosClientes, ConverterEmCliente);
         }
+        private Dictionary<string, object> ObtemParametrosClientes(Clientes clientes)
+        {
+            var parametros = new Dictionary<string, object>();
+
+            parametros.Add("ID", clientes.Id);
+            parametros.Add("NOME", clientes.Nome);
+            parametros.Add("ENDERECO", clientes.Endereco);
+            parametros.Add("TELEFONE", clientes.Telefone);
+            parametros.Add("RG", clientes.RG);
+            parametros.Add("CPF", clientes.CPF);
+            parametros.Add("CNPJ", clientes.CNPJ);
+
+            return parametros;
+        }
+        private Clientes ConverterEmCliente(IDataReader reader)
+        {
+            int id = Convert.ToInt32(reader["ID"]);
+            string nome = Convert.ToString(reader["NOME"]);
+            string endereco = Convert.ToString(reader["ENDERECO"]);
+            string telefone = Convert.ToString(reader["TELEFONE"]);
+            string rg = Convert.ToString(reader["RG"]);
+            string cpf = Convert.ToString(reader["CPF"]);
+            string cnpj = Convert.ToString(reader["CNPJ"]);
+
+            Clientes clientes = new Clientes(nome, endereco, telefone, rg, cpf, cnpj);
+
+            clientes.Id = id;
+
+            return clientes;
+        }
+
     }
 }
