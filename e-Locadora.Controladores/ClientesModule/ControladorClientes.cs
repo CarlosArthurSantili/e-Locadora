725e3d4a6c8f4b63cbf3a3bcc2718aa5c0162101
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-10 15:52:21 -0300

    criando queris com comandos sql

--- e-Locadora.Controladores/ClientesModule/ControladorClientes.cs
+++ e-Locadora.Controladores/ClientesModule/ControladorClientes.cs
@@ -10,6 +10,79 @@ namespace e_Locadora.Controladores.ClientesModule
 {
     public class ControladorClientes : Controlador<Clientes>
     {
+        #region
+        private const string sqlInserirCliente =
+            @"INSERT INTO TBCLIENTES 
+	                (
+		                [NOME], 
+		                [ENDERECO], 
+		                [TELEFONE],
+                        [RG], 
+		                [CPF],
+                        [CNPJ]
+	                ) 
+	                VALUES
+	                (
+                        @NOME, 
+		                @ENDERECO, 
+		                @TELEFONE,
+                        @RG, 
+		                @CPF,
+                        @CNPJ
+	                )";
+
+        private const string sqlEditarCliente =
+            @"UPDATE TBCLIENTES
+                    SET
+                        [NOME] = @NOME, 
+		                [ENDERECO] = @ENDERECO, 
+		                [TELEFONE] = @TELEFONE,
+                        [RG] = @RG, 
+		                [CPF] = @CPF,
+                        [CNPJ] = @CNPJ
+                    WHERE 
+                        ID = @ID";
+
+        private const string sqlExcluirCliente =
+            @"DELETE 
+	                FROM
+                        TBCLIENTES
+                    WHERE 
+                        ID = @ID";
+
+        private const string sqlSelecionarClientePorId =
+            @"SELECT
+                        [ID],
+                        [NOME], 
+		                [ENDERECO], 
+		                [TELEFONE],
+                        [RG], 
+		                [CPF],
+                        [CNPJ]
+	                FROM
+                        TBCLIENTES
+                    WHERE 
+                        ID = @ID";
+
+        private const string sqlSelecionarTodosClientes =
+            @"SELECT
+                        [ID],
+                        [NOME], 
+		                [ENDERECO], 
+		                [TELEFONE],
+                        [RG], 
+		                [CPF],
+                        [CNPJ] FROM TBCLIENTES";
+
+        private const string sqlExisteCliente =
+            @"SELECT 
+                COUNT(*) 
+            FROM 
+                [TBCLIENTES]
+            WHERE 
+                [ID] = @ID";
+
+        #endregion
         public override string InserirNovo(Clientes registro)
         {
             throw new NotImplementedException();
