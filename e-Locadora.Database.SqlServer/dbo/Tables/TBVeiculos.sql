0efb9f89dcb90970b0f2a01c0afd843264538278
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-08-11 13:12:30 -0300

    Alterações no banco de dados para se adaptar ao dominio já existente.

--- e-Locadora.Database.SqlServer/dbo/Tables/TBVeiculos.sql
+++ e-Locadora.Database.SqlServer/dbo/Tables/TBVeiculos.sql
@@ -6,12 +6,12 @@
     [NumeroChassi]        VARCHAR (50) NOT NULL,
     [Cor]                 VARCHAR (50) NOT NULL,
     [CapacidadeOcupantes] INT          NOT NULL,
-    [AnoDeFabricação]     INT          NOT NULL,
-    [TamanhoPortaMalas]   INT          NOT NULL,
+    [AnoFabricacao]     INT          NOT NULL,
+    [TamanhoPortaMalas]   VARCHAR(50)          NOT NULL,
     [TipoCombustivel]     VARCHAR (50) NOT NULL,
-    [GrupoVeiculo]        INT          NOT NULL,
-    [Foto] VARBINARY(MAX) NULL, 
+    [idGrupoVeiculo]        INT          NOT NULL,
+    [Foto] IMAGE NULL, 
     CONSTRAINT [PK_TBVeiculos] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_TBVeiculos_Categorias] FOREIGN KEY ([GrupoVeiculo]) REFERENCES [dbo].[Categorias] ([Id])
+    CONSTRAINT [FK_TBVeiculos_Categorias] FOREIGN KEY ([idGrupoVeiculo]) REFERENCES [dbo].[Categorias] ([Id])
 );
 
