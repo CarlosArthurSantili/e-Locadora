7714de546b29d97d056d2ae4050f4bef5f55e529
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-10 21:21:17 -0300

    Tela Cliente Forms Concluida

--- e-Locadora.WindowsApp/ClientesModule/TelaClientesForm.Designer.cs
+++ e-Locadora.WindowsApp/ClientesModule/TelaClientesForm.Designer.cs
@@ -29,12 +29,242 @@ namespace e_Locadora.WindowsApp.ClientesModule
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.TxtTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtRG = new System.Windows.Forms.MaskedTextBox();
+            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.txtCnpj = new System.Windows.Forms.MaskedTextBox();
+            this.rbCPF = new System.Windows.Forms.RadioButton();
+            this.rbCNPJ = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(318, 265);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 27;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnGravar.Location = new System.Drawing.Point(237, 265);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 26;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Número do CPF";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Número do RG";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Telefone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Endereço Completo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Id";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(121, 121);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(165, 20);
+            this.txtEndereco.TabIndex = 17;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(121, 95);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(165, 20);
+            this.txtNome.TabIndex = 16;
+            // 
+            // txtId
+            // 
+            this.txtId.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(121, 69);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(48, 20);
+            this.txtId.TabIndex = 15;
+            // 
+            // TxtTelefone
+            // 
+            this.TxtTelefone.Location = new System.Drawing.Point(121, 147);
+            this.TxtTelefone.Mask = "(99) 00000-0000";
+            this.TxtTelefone.Name = "TxtTelefone";
+            this.TxtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.TxtTelefone.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Número do CNPJ";
+            // 
+            // txtRG
+            // 
+            this.txtRG.Location = new System.Drawing.Point(121, 173);
+            this.txtRG.Mask = "000000000";
+            this.txtRG.Name = "txtRG";
+            this.txtRG.Size = new System.Drawing.Size(100, 20);
+            this.txtRG.TabIndex = 29;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(121, 199);
+            this.txtCPF.Mask = "000.000.000-00";
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(100, 20);
+            this.txtCPF.TabIndex = 30;
+            // 
+            // txtCnpj
+            // 
+            this.txtCnpj.Location = new System.Drawing.Point(121, 225);
+            this.txtCnpj.Mask = "00000000/0000-00";
+            this.txtCnpj.Name = "txtCnpj";
+            this.txtCnpj.Size = new System.Drawing.Size(100, 20);
+            this.txtCnpj.TabIndex = 31;
+            // 
+            // rbCPF
+            // 
+            this.rbCPF.AutoSize = true;
+            this.rbCPF.Checked = true;
+            this.rbCPF.Location = new System.Drawing.Point(18, 24);
+            this.rbCPF.Name = "rbCPF";
+            this.rbCPF.Size = new System.Drawing.Size(152, 17);
+            this.rbCPF.TabIndex = 32;
+            this.rbCPF.TabStop = true;
+            this.rbCPF.Text = "Cadastro de Pessoa Física";
+            this.rbCPF.UseVisualStyleBackColor = true;
+            this.rbCPF.CheckedChanged += new System.EventHandler(this.rbCPF_CheckedChanged);
+            // 
+            // rbCNPJ
+            // 
+            this.rbCNPJ.AutoSize = true;
+            this.rbCNPJ.Location = new System.Drawing.Point(234, 24);
+            this.rbCNPJ.Name = "rbCNPJ";
+            this.rbCNPJ.Size = new System.Drawing.Size(159, 17);
+            this.rbCNPJ.TabIndex = 33;
+            this.rbCNPJ.Text = "Cadastro de Pessoa Juridica";
+            this.rbCNPJ.UseVisualStyleBackColor = true;
+            this.rbCNPJ.CheckedChanged += new System.EventHandler(this.rbCNPJ_CheckedChanged);
+            // 
+            // TelaClientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "TelaClientesForm";
+            this.ClientSize = new System.Drawing.Size(405, 300);
+            this.Controls.Add(this.rbCNPJ);
+            this.Controls.Add(this.rbCPF);
+            this.Controls.Add(this.txtCnpj);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtRG);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.TxtTelefone);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaClientesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tela de Cadastro de Clientes";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TelaClientesForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.MaskedTextBox TxtTelefone;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox txtRG;
+        private System.Windows.Forms.MaskedTextBox txtCPF;
+        private System.Windows.Forms.MaskedTextBox txtCnpj;
+        private System.Windows.Forms.RadioButton rbCPF;
+        private System.Windows.Forms.RadioButton rbCNPJ;
     }
 }
--- e-Locadora.WindowsApp/ClientesModule/TelaClientesForm.cs
+++ e-Locadora.WindowsApp/ClientesModule/TelaClientesForm.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using e_Locadora.Dominio.ClientesModule;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,74 @@ namespace e_Locadora.WindowsApp.ClientesModule
 {
     public partial class TelaClientesForm : Form
     {
+        private Clientes cliente;
         public TelaClientesForm()
         {
             InitializeComponent();
+        }
+        public Clientes Cliente
+        {
+            get { return cliente; }
+
+            set
+            {
+                cliente = value;
+
+                txtId.Text = cliente.Id.ToString();
+                txtNome.Text = cliente.Nome;
+                txtEndereco.Text = cliente.Endereco;
+
+                TxtTelefone.Text = cliente.Telefone;
+                txtRG.Text = cliente.RG;
+                txtCPF.Text = cliente.CPF;
+                txtCnpj.Text = cliente.CNPJ;
+            }
+        }
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string endereco = txtEndereco.Text;
+            string telefone = TxtTelefone.Text;
+            string rg = txtRG.Text;
+            string cpf = txtCPF.Text;
+            string cnpj = txtCnpj.Text;
+
+            cliente = new Clientes(nome, endereco, telefone, rg, cpf, cnpj);
+
+            string resultadoValidacao = cliente.Validar();
+
+            if (resultadoValidacao != "ESTA_VALIDO")
+            {
+                string primeiroErro = new StringReader(resultadoValidacao).ReadLine();
+
+                TelaPrincipalForm.Instancia.AtualizarRodape(primeiroErro);
+
+                DialogResult = DialogResult.None;
+            }
+        }
+        private void rbCPF_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbCPF.Checked)
+            {
+                txtCnpj.Enabled = false;
+                txtCPF.Enabled = true;
+                txtRG.Enabled = true;
+            }
+        }
+
+        private void rbCNPJ_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbCNPJ.Checked)
+            {
+                txtCnpj.Enabled = true;
+                txtCPF.Enabled = false;
+                txtRG.Enabled = false;
+            }
+        }
+
+        private void TelaClientesForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TelaPrincipalForm.Instancia.AtualizarRodape("");
         }
     }
 }
