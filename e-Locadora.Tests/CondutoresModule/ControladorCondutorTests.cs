4c50be15aca504f398112d1a3c699d96fe79c75e
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-08-12 17:17:06 -0300

    Merge Condutores 1.1

---
+++ e-Locadora.Tests/CondutoresModule/ControladorCondutorTests.cs
@@ -0,0 +1,147 @@
+﻿using e_Locadora.Controladores;
+using e_Locadora.Controladores.ClientesModule;
+using e_Locadora.Controladores.CondutorModule;
+using e_Locadora.Dominio.ClientesModule;
+using e_Locadora.Dominio.CondutoresModule;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace e_Locadora.Tests.CondutoresModule
+{
+    [TestClass]
+    [TestCategory("Controladores")]
+    public class ControladorCondutorTests
+    {
+        ControladorCondutor controladorCondutor = null;
+        ControladorClientes controladorCliente = null;
+
+        public ControladorCondutorTests()
+        {
+            controladorCondutor = new ControladorCondutor();
+            controladorCliente = new ControladorClientes();
+
+            Db.Update("DELETE FROM [TBCONDUTOR]");
+            Db.Update("DELETE FROM [TBCLIENTES]");
+        }
+
+        [TestMethod]
+        public void Deve_InserirUmCondutor()
+        {
+            Clientes cliente = new Clientes("Arnaldo", "rua sem numero", "9524282242", "853242", "20220220222", "");
+
+            controladorCliente.InserirNovo(cliente);
+
+            Condutor condutor = new Condutor("Joao", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",
+                new DateTime(2022, 05, 26), cliente);
+
+            controladorCondutor.InserirNovo(condutor);
+
+
+            var condutorEncontrado = controladorCondutor.SelecionarPorId(condutor.Id);
+            condutorEncontrado.Should().Be(condutor);
+
+        }
+        [TestMethod]
+        public void DeveAtualizar_Condutor()
+        {
+            Clientes cliente = new Clientes("Arnaldo", "rua sem numero", "9524282242", "853242", "20220220222", "");
+
+            controladorCliente.InserirNovo(cliente);
+
+            Condutor condutor = new Condutor("Joao", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",
+                new DateTime(2022, 05, 26), cliente);
+
+            controladorCondutor.InserirNovo(condutor);
+
+            var condutorEditado = new Condutor("Joao", "rua sssds", "9522185224", "5222522", "20202020222", "522542",
+                new DateTime(2022, 05, 22), cliente);
+
+            controladorCondutor.Editar(condutor.Id, condutorEditado);
+
+            Condutor condutorEncontrado = controladorCondutor.SelecionarPorId(condutor.Id);
+            condutorEncontrado.Should().Be(condutorEditado);
+
+        }
+        [TestMethod]
+        public void DeveExcluir_Condutor()
+        {
+            Clientes cliente = new Clientes("Arnaldo", "rua sem numero", "9524282242", "853242", "20220220222", "");
+
+            controladorCliente.InserirNovo(cliente);
+
+            Condutor condutor = new Condutor("Joao", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",
+                new DateTime(2022, 05, 26), cliente);
+
+            controladorCondutor.InserirNovo(condutor);
+
+            controladorCondutor.Excluir(condutor.Id);
+
+            Condutor condutorEncontrado = controladorCondutor.SelecionarPorId(condutor.Id);
+            condutorEncontrado.Should().BeNull();
+        }
+        [TestMethod]
+        public void DeveSelecionar_Condutor_PorId()
+        {
+            Clientes cliente = new Clientes("Arnaldo", "rua sem numero", "9524282242", "853242", "20220220222", "");
+
+            controladorCliente.InserirNovo(cliente);
+
+            Condutor condutor = new Condutor("Joao", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",
+                new DateTime(2022, 05, 26), cliente);
+
+            controladorCondutor.InserirNovo(condutor);
+
+            Condutor condutor1 = controladorCondutor.SelecionarPorId(condutor.Id);
+            condutor1.Should().NotBeNull();
+
+        }
+        [TestMethod]
+        public void DeveSelecionar_TodosCondutores()
+        {
+            Clientes cliente = new Clientes("Arnaldo", "rua sem numero", "9524282242", "853242", "20220220222", "");
+
+            controladorCliente.InserirNovo(cliente);
+            var condutores = new List<Condutor>
+            {
+                new Condutor("Joao", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2022, 05, 26), cliente),
+                new Condutor("marcelo", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2022, 05, 26), cliente),
+                new Condutor("carlos", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2022, 05, 26), cliente),
+                new Condutor("ze", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2022, 05, 26), cliente),
+                new Condutor("Bastiao", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2022, 05, 26), cliente)
+
+            };
+            foreach (var c in condutores)
+                controladorCondutor.InserirNovo(c);
+
+            var condutoreSelecionado = controladorCondutor.SelecionarTodos();
+
+            condutoreSelecionado.Should().HaveCount(5);
+
+        }
+        [TestMethod]
+        public void DeveSelecionar_Condutores_Com_CNH_Vencida()
+        {
+            Clientes cliente = new Clientes("Arnaldo", "rua sem numero", "9524282242", "853242", "20220220222", "");
+
+            controladorCliente.InserirNovo(cliente);
+            var condutores = new List<Condutor>
+            {
+                new Condutor("Joao", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2022, 05, 26), cliente),
+                new Condutor("marcelo", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2022, 05, 26), cliente),
+                new Condutor("carlos", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2021, 08, 10), cliente),
+                new Condutor("ze", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2022, 05, 26), cliente),
+                new Condutor("Bastiao", "Rua dos joao", "9522185224", "5222522", "20202020222", "522542",new DateTime(2022, 05, 26), cliente)
+
+            };
+            foreach (var c in condutores)
+                controladorCondutor.InserirNovo(c);
+
+            DateTime hoje = new DateTime(2021, 8, 12);
+            var CnhVencida = controladorCondutor.SelecionarCondutoresComCnhVencida(hoje);
+
+            CnhVencida.Should().HaveCount(1);
+        }
+    }
+}
