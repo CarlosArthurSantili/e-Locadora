7868274a5da6bc918b31d3ef7833ee64d977433f
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-08-13 16:59:07 -0300

    alteração na ordem de exibição da tabela

--- e-Locadora.WindowsApp/Features/GrupoVeiculoModule/TabelaGrupoVeiculoControl.cs
+++ e-Locadora.WindowsApp/Features/GrupoVeiculoModule/TabelaGrupoVeiculoControl.cs
@@ -37,11 +37,11 @@ namespace e_Locadora.WindowsApp.GrupoVeiculoModule
 
                 new DataGridViewTextBoxColumn { DataPropertyName = "planoDiarioValorDiario", HeaderText = "PD: Valor Km"},
 
-                new DataGridViewTextBoxColumn { DataPropertyName = "planoKmControladoQuantidadeKm", HeaderText = "PC: Quantidade de Km"},
+                new DataGridViewTextBoxColumn { DataPropertyName = "planoKmControladoValorDiario", HeaderText = "PC: Valor Km"},
 
                 new DataGridViewTextBoxColumn { DataPropertyName = "planoKmControladoValorKm", HeaderText = "PC: Valor Diário"},
 
-                new DataGridViewTextBoxColumn { DataPropertyName = "planoKmControladoValorDiario", HeaderText = "PC: Valor Km"},
+                new DataGridViewTextBoxColumn { DataPropertyName = "planoKmControladoQuantidadeKm", HeaderText = "PC: Quantidade de Km"},
 
                 new DataGridViewTextBoxColumn { DataPropertyName = "planoKmLivreValorDiario", HeaderText = "PL: Valor Diário"}
             };
