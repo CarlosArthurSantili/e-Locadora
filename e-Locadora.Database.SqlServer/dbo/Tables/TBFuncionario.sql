671781036fec3c044d33f5eff74556598dea9fa7
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-17 15:57:01 -0300

    Acresentado Tabela funcionario aos DB locaradora e locadora tests

---
+++ e-Locadora.Database.SqlServer/dbo/Tables/TBFuncionario.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBFuncionario] (
+    [Id]           INT          IDENTITY (1, 1) NOT NULL,
+    [Nome]         VARCHAR (80) NOT NULL,
+    [Usuario]      VARCHAR (80) NOT NULL,
+    [Senha]        VARCHAR (80) NOT NULL,
+    [DataAdmissao] DATE         NOT NULL,
+    [Salario]      NUMERIC (18) NOT NULL,
+    CONSTRAINT [PK_TBFuncionario] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
