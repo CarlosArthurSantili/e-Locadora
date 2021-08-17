5cbe8b011a986e944960f8199bd5689bbf47e8f6
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-17 14:06:13 -0300

    Criado Pasta Funcionrio com a clase dentro do dominio feita metodo construtor

---
+++ e-Locadora.Dominio/FuncionarioModule/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using e_Locadora.Dominio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora.Dominio.FuncionarioModule
+{
+    public class Funcionario : EntidadeBase
+    {
+        public string Nome { get; }
+        public string Usuario { get; }
+        public string Senha { get; }
+        public DateTime DataAdimicao { get; }
+        public double Salario { get; }
+
+        public Funcionario(string nome, string usuario, string senha, DateTime dataAdimicao, double salario)
+        {
+            Nome = nome;
+            Usuario = usuario;
+            Senha = senha;
+            DataAdimicao = dataAdimicao;
+            Salario = salario;
+        }
+
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
