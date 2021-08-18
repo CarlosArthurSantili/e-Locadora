89723ee35f3182aeb233448cafdb933f07c1bf60
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-08-18 17:00:15 -0300

    Implementado OperacoesLocacao.

--- e-Locadora.WindowsApp/Features/LocacaoModule/OperacoesLocacao.cs
+++ e-Locadora.WindowsApp/Features/LocacaoModule/OperacoesLocacao.cs
@@ -1,4 +1,5 @@
 ﻿using e_Locadora.Controladores.LocacaoModule;
+using e_Locadora.Dominio.LocacaoModule;
 using e_Locadora.WindowsApp.Shared;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,84 @@ namespace e_Locadora.WindowsApp.Features.LocacaoModule
 {
     public class OperacoesLocacao : ICadastravel
     {
-        private ControladorLocacao controlador = null;
+        private ControladorLocacao controladorLocacao = null;
+
+        public OperacoesLocacao(ControladorLocacao controladorLocacao)
+        {
+            this.controladorLocacao = controladorLocacao;
+            tabelaLocacao = new TabelaLocacaoControl(controladorLocacao);
+        }
+        public void InserirNovoRegistro()
+        {
+            TelaLocacaoForm tela = new TelaLocacaoForm();
+            tela.ShowDialog();
+            if (tela.DialogResult == DialogResult.OK && controladorLocacao.ValidarLocacao(tela.Locacao) == "ESTA_VALIDO")
+            {
+                controladorLocacao.InserirNovo(tela.Locacao);
+
+                tabelaLocacao.AtualizarRegistros();
+
+                TelaPrincipalForm.Instancia.AtualizarRodape($"Locação do veículo: [{tela.Locacao.veiculo.modelo}] para o Cliente: [{tela.Locacao.cliente.nome}] foi efetuada com sucesso");
+            }
+        }
+
+        public void EditarRegistro()
+        {
+            int id = tabelaLocacao.ObtemIdSelecionado();
+
+            if (id == 0)
+            {
+                MessageBox.Show("Selecione um Locação para poder editar!", "Edição de Locacao",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            Locacao clienteSelecionado = controladorLocacao.SelecionarPorId(id);
+
+            TelaLocacaoForm tela = new TelaLocacaoForm();
+
+            tela.Locacao = clienteSelecionado;
+            tela.ShowDialog();
+            if (tela.DialogResult == DialogResult.OK && controladorLocacao.ValidarLocacao(tela.Locacao, id) == "ESTA_VALIDO")
+            {
+                controladorLocacao.Editar(id, tela.Locacao);
+
+                tabelaLocacao.AtualizarRegistros();
+
+                TelaPrincipalForm.Instancia.AtualizarRodape($"Locação do veículo: [{tela.Locacao.veiculo.modelo}] para o Cliente: [{tela.Locacao.cliente.nome}] foi editada com sucesso");
+            }
+        }
+
+        public void ExcluirRegistro()
+        {
+            int id = tabelaLocacao.ObtemIdSelecionado();
+
+            if (id == 0)
+            {
+                MessageBox.Show("Selecione um Locação para poder excluir!", "Exclusão de Locacao",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            Locacao locacaoSelecionado = controladorLocacao.SelecionarPorId(id);
+
+            if (MessageBox.Show($"Tem certeza que deseja excluir a Locação do veículo: [{locacaoSelecionado.veiculo.Modelo}] para o Cliente: [{tela.Locacao.cliente.nome}]?",
+                "Exclusão de Locação", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                controladorLocacao.Excluir(id);
+
+                tabelaLocacao.AtualizarRegistros();
+
+                TelaPrincipalForm.Instancia.AtualizarRodape($"Locação do veículo: [{tela.Locacao.veiculo.modelo}] para o Cliente: [{tela.Locacao.cliente.nome}] foi removida com sucesso");
+            }
+        }
+
+        public UserControl ObterTabela()
+        {
+            tabelaLocacao.AtualizarRegistros();
+
+            return tabelaLocacao;
+        }
 
         public void AgruparRegistros()
         {
@@ -23,29 +101,13 @@ namespace e_Locadora.WindowsApp.Features.LocacaoModule
             throw new NotImplementedException();
         }
 
-        public void EditarRegistro()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ExcluirRegistro()
-        {
-            throw new NotImplementedException();
-        }
-
         public void FiltrarRegistros()
         {
             throw new NotImplementedException();
         }
 
-        public void InserirNovoRegistro()
-        {
+        
 
-        }
-
-        public UserControl ObterTabela()
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
