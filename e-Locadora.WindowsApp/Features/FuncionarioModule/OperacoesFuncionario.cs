169025a8bf7f2dc6c59b8c6685173896cb85d530
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-18 14:22:00 -0300

    Implementado Tabela control de funcionario

--- e-Locadora.WindowsApp/Features/FuncionarioModule/OperacoesFuncionario.cs
+++ e-Locadora.WindowsApp/Features/FuncionarioModule/OperacoesFuncionario.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using e_Locadora.WindowsApp.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace e_Locadora.WindowsApp.Features.FuncionarioModule
 {
-    class OperacoesFuncionario
+    public class OperacoesFuncionario : ICadastravel
     {
+        public void InserirNovoRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditarRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserControl ObterTabela()
+        {
+            throw new NotImplementedException();
+        }
+        public void FiltrarRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AgruparRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DesagruparRegistros()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- e-Locadora.WindowsApp/Features/FuncionarioModule/TelaFuncionarioControl.cs
+++ e-Locadora.WindowsApp/Features/FuncionarioModule/TelaFuncionarioControl.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using e_Locadora.Controladores.FuncionarioModule;
+using e_Locadora.Dominio.FuncionarioModule;
+using e_Locadora.WindowsApp.Shared;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,11 +15,54 @@ namespace e_Locadora.WindowsApp.Features.FuncionarioModule
 {
     public partial class TelaFuncionarioControl : UserControl
     {
-        public TelaFuncionarioControl()
+        private readonly ControladorFuncionario controladorFuncionario;
+        public TelaFuncionarioControl(ControladorFuncionario controladorFuncionario)
         {
             InitializeComponent();
+            gridFuncionario.ConfigurarGridZebrado();
+            gridFuncionario.ConfigurarGridSomenteLeitura();
+            gridFuncionario.Columns.AddRange(ObterColunas());
+            this.controladorFuncionario = controladorFuncionario;
         }
 
-     
+        private DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+           {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Nome", HeaderText = "Nome"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "NumeroCpf", HeaderText = "Numero CPF"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Usuario", HeaderText = "Login"},
+
+                new DataGridViewTextBoxColumn {DataPropertyName = "Senha", HeaderText = "Senha de Aceso"},
+
+                new DataGridViewTextBoxColumn {DataPropertyName = "DataAdmissao", HeaderText = "Data de Admissão"},
+
+                 new DataGridViewTextBoxColumn {DataPropertyName = "Salario", HeaderText = "Sálario Mensal"}
+           };
+
+            return colunas;
+        }
+        public int ObtemIdSelecionado()
+        {
+            return gridFuncionario.SelecionarId<int>();
+        }
+
+        public void AtualizarRegistros()
+        {
+
+            var funcionarios = controladorFuncionario.SelecionarTodos();
+
+            CarregarTbela(funcionarios);
+
+        }
+
+        private void CarregarTbela(List<Funcionario> funcionarios)
+        {
+            gridFuncionario.DataSource = funcionarios;
+        }
     }
 }
