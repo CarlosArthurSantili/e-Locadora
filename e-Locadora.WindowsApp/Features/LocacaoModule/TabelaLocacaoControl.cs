330f2aae262f52a4b13a62036d1137c5d3b72da1
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-08-18 15:33:06 -0300

    Criação de ConfiguracaoToolBox, OperacaoesLocacoes e TelaLocação

--- e-Locadora.WindowsApp/Features/LocacaoModule/TabelaLocacaoControl.cs
+++ e-Locadora.WindowsApp/Features/LocacaoModule/TabelaLocacaoControl.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using e_Locadora.Controladores.LocacaoModule;
+using e_Locadora.Dominio.LocacaoModule;
+using e_Locadora.WindowsApp.Shared;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,56 @@ namespace e_Locadora.WindowsApp.Features.LocacaoModule
 {
     public partial class TabelaLocacaoControl : UserControl
     {
+
+        public ControladorLocacao controladorLocacao = new ControladorLocacao();
+
+
         public TabelaLocacaoControl()
         {
             InitializeComponent();
+            gridLocacao.ConfigurarGridZebrado();
+            gridLocacao.ConfigurarGridSomenteLeitura();
+            gridLocacao.Columns.AddRange(ObterColunas());
+        }
+
+        private DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+{
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "funcionatio", HeaderText = "Funcionario"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "cliente", HeaderText = "Cliente"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "veiculo", HeaderText = "Veiculo"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "condutor", HeaderText = "Condutor"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "dataLocacao", HeaderText = "Data de Locação"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "dataDevolucao", HeaderText = "Data de Devolução"},
+
+};
+
+            return colunas;
+        }
+
+        public int ObtemIdSelecionado()
+        {
+            return gridLocacao.SelecionarId<int>();
+        }
+
+        public void AtualizarRegistros()
+        {
+            var locacao = controladorLocacao.SelecionarTodos();
+
+            CarregarTabela(locacao);
+        }
+
+        public void CarregarTabela(List<Locacao> locacaos)
+        {
+            gridLocacao.DataSource = locacaos;
         }
     }
 }
