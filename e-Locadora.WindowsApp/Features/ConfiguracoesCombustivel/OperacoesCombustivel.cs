1e4a565c2cf712de79a0ee157cdbb5ecf5b44dfc
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-19 15:17:18 -0300

    adionado classes de combustivel

---
+++ e-Locadora.WindowsApp/Features/ConfiguracoesCombustivel/OperacoesCombustivel.cs
@@ -0,0 +1,49 @@
+﻿using e_Locadora.WindowsApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
+{
+    public class OperacoesCombustivel : ICadastravel
+    {
+        public UserControl ObterTabela()
+        {
+            throw new NotImplementedException();
+        }
+        public void AgruparRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DesagruparRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditarRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FiltrarRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InserirNovoRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+ 
+    }
+}
