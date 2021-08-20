7470eeedc59691847ad755103591ee1aff31836e
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-20 15:12:08 -0300

    Implementado Classe enum

---
+++ e-Locadora.WindowsApp/Features/LocacaoModule/TelaFiltroLocacaoForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace e_Locadora.WindowsApp.Features.LocacaoModule
+{
+    partial class TelaFiltroLocacaoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.rbCnhVencida = new System.Windows.Forms.RadioButton();
+            this.rbTodos = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(197, 120);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnGravar.Location = new System.Drawing.Point(116, 120);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 10;
+            this.btnGravar.Text = "Filtrar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // rbCnhVencida
+            // 
+            this.rbCnhVencida.AutoSize = true;
+            this.rbCnhVencida.Location = new System.Drawing.Point(54, 70);
+            this.rbCnhVencida.Name = "rbCnhVencida";
+            this.rbCnhVencida.Size = new System.Drawing.Size(176, 17);
+            this.rbCnhVencida.TabIndex = 9;
+            this.rbCnhVencida.TabStop = true;
+            this.rbCnhVencida.Text = "Visualizar Locações Pendentes ";
+            this.rbCnhVencida.UseVisualStyleBackColor = true;
+            // 
+            // rbTodos
+            // 
+            this.rbTodos.AutoSize = true;
+            this.rbTodos.Location = new System.Drawing.Point(54, 25);
+            this.rbTodos.Name = "rbTodos";
+            this.rbTodos.Size = new System.Drawing.Size(152, 17);
+            this.rbTodos.TabIndex = 8;
+            this.rbTodos.TabStop = true;
+            this.rbTodos.Text = "Visualizar Todas Locações";
+            this.rbTodos.UseVisualStyleBackColor = true;
+            // 
+            // TelaFiltroLocacaoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 162);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.rbCnhVencida);
+            this.Controls.Add(this.rbTodos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaFiltroLocacaoForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Filtro Locações Pendentes ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.RadioButton rbCnhVencida;
+        private System.Windows.Forms.RadioButton rbTodos;
+    }
+}
