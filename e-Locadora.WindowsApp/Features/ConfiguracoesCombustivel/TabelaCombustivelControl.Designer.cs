88f01c5988b037a1786ba387b125745039f1a1a4
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-08-24 15:46:54 -0300

    Edição dos preços de combustíveis agora não fecha a tela principal

--- e-Locadora.WindowsApp/Features/ConfiguracoesCombustivel/TabelaCombustivelControl.Designer.cs
+++ e-Locadora.WindowsApp/Features/ConfiguracoesCombustivel/TabelaCombustivelControl.Designer.cs
@@ -39,13 +39,15 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             this.txtGas = new System.Windows.Forms.TextBox();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.btnGravar = new System.Windows.Forms.Button();
+            this.groupBoxCombustiveis = new System.Windows.Forms.GroupBox();
+            this.groupBoxCombustiveis.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(41, 86);
+            this.label1.Location = new System.Drawing.Point(8, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(97, 25);
             this.label1.TabIndex = 0;
@@ -55,7 +57,7 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(67, 122);
+            this.label2.Location = new System.Drawing.Point(34, 59);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 25);
             this.label2.TabIndex = 1;
@@ -65,7 +67,7 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(66, 158);
+            this.label3.Location = new System.Drawing.Point(33, 95);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(72, 25);
             this.label3.TabIndex = 2;
@@ -75,7 +77,7 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(82, 194);
+            this.label4.Location = new System.Drawing.Point(49, 131);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 25);
             this.label4.TabIndex = 3;
@@ -84,7 +86,7 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             // txtGasolina
             // 
             this.txtGasolina.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtGasolina.Location = new System.Drawing.Point(161, 82);
+            this.txtGasolina.Location = new System.Drawing.Point(128, 19);
             this.txtGasolina.Name = "txtGasolina";
             this.txtGasolina.Size = new System.Drawing.Size(120, 29);
             this.txtGasolina.TabIndex = 4;
@@ -92,7 +94,7 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             // txtAlcool
             // 
             this.txtAlcool.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAlcool.Location = new System.Drawing.Point(161, 118);
+            this.txtAlcool.Location = new System.Drawing.Point(128, 55);
             this.txtAlcool.Name = "txtAlcool";
             this.txtAlcool.Size = new System.Drawing.Size(120, 29);
             this.txtAlcool.TabIndex = 5;
@@ -100,7 +102,7 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             // txtDiesel
             // 
             this.txtDiesel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDiesel.Location = new System.Drawing.Point(161, 154);
+            this.txtDiesel.Location = new System.Drawing.Point(128, 91);
             this.txtDiesel.Name = "txtDiesel";
             this.txtDiesel.Size = new System.Drawing.Size(120, 29);
             this.txtDiesel.TabIndex = 6;
@@ -108,15 +110,14 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             // txtGas
             // 
             this.txtGas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtGas.Location = new System.Drawing.Point(161, 190);
+            this.txtGas.Location = new System.Drawing.Point(128, 127);
             this.txtGas.Name = "txtGas";
             this.txtGas.Size = new System.Drawing.Size(120, 29);
             this.txtGas.TabIndex = 7;
             // 
             // btnCancelar
             // 
-            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancelar.Location = new System.Drawing.Point(286, 256);
+            this.btnCancelar.Location = new System.Drawing.Point(195, 175);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 23);
             this.btnCancelar.TabIndex = 10;
@@ -126,8 +127,7 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             // 
             // btnGravar
             // 
-            this.btnGravar.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnGravar.Location = new System.Drawing.Point(205, 256);
+            this.btnGravar.Location = new System.Drawing.Point(114, 175);
             this.btnGravar.Name = "btnGravar";
             this.btnGravar.Size = new System.Drawing.Size(75, 23);
             this.btnGravar.TabIndex = 9;
@@ -135,24 +135,35 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
             this.btnGravar.UseVisualStyleBackColor = true;
             this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
             // 
+            // groupBoxCombustiveis
+            // 
+            this.groupBoxCombustiveis.Controls.Add(this.txtGasolina);
+            this.groupBoxCombustiveis.Controls.Add(this.label1);
+            this.groupBoxCombustiveis.Controls.Add(this.label2);
+            this.groupBoxCombustiveis.Controls.Add(this.txtGas);
+            this.groupBoxCombustiveis.Controls.Add(this.label3);
+            this.groupBoxCombustiveis.Controls.Add(this.txtDiesel);
+            this.groupBoxCombustiveis.Controls.Add(this.label4);
+            this.groupBoxCombustiveis.Controls.Add(this.txtAlcool);
+            this.groupBoxCombustiveis.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxCombustiveis.Name = "groupBoxCombustiveis";
+            this.groupBoxCombustiveis.Size = new System.Drawing.Size(270, 169);
+            this.groupBoxCombustiveis.TabIndex = 11;
+            this.groupBoxCombustiveis.TabStop = false;
+            this.groupBoxCombustiveis.Text = "Combustíveis";
+            // 
             // TabelaCombustivelControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBoxCombustiveis);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnGravar);
-            this.Controls.Add(this.txtGas);
-            this.Controls.Add(this.txtDiesel);
-            this.Controls.Add(this.txtAlcool);
-            this.Controls.Add(this.txtGasolina);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "TabelaCombustivelControl";
-            this.Size = new System.Drawing.Size(401, 303);
+            this.Size = new System.Drawing.Size(283, 205);
+            this.groupBoxCombustiveis.ResumeLayout(false);
+            this.groupBoxCombustiveis.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -168,5 +179,6 @@ namespace e_Locadora.WindowsApp.Features.ConfiguracoesCombustivel
         private System.Windows.Forms.TextBox txtGas;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.GroupBox groupBoxCombustiveis;
     }
 }
