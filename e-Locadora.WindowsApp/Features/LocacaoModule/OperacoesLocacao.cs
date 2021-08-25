336d0d621e855c88e199a4214ddb3758a22261fb
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-08-25 16:40:43 -0300

    Não é possível realizar o cadastro de uma locação para um condutor que possui a cnh vencida, porém é possível realizar a devolução de um condutor que possui a cnh vencida*

--- e-Locadora.WindowsApp/Features/LocacaoModule/OperacoesLocacao.cs
+++ e-Locadora.WindowsApp/Features/LocacaoModule/OperacoesLocacao.cs
@@ -32,7 +32,7 @@ namespace e_Locadora.WindowsApp.Features.LocacaoModule
             TelaLocacaoForm tela = new TelaLocacaoForm();
             tela.ShowDialog();
             if (tela.DialogResult == DialogResult.OK && controladorLocacao.ValidarLocacao(tela.Locacao) == "ESTA_VALIDO" 
-                && controladorLocacao.ValidarCnh(tela.Locacao) == "ESTA_VALIDO")
+                && controladorLocacao.ValidarCNH(tela.Locacao) == "ESTA_VALIDO")
             {
                 controladorLocacao.InserirNovo(tela.Locacao);
 
@@ -60,7 +60,7 @@ namespace e_Locadora.WindowsApp.Features.LocacaoModule
             tela.Locacao = locacaoSelecionado;
             tela.ShowDialog();
             if (tela.DialogResult == DialogResult.OK && controladorLocacao.ValidarLocacao(tela.Locacao, id) == "ESTA_VALIDO"
-                && controladorLocacao.ValidarCnh(tela.Locacao) == "ESTA_VALIDO")
+                && controladorLocacao.ValidarCNH(tela.Locacao) == "ESTA_VALIDO")
             {
      
                 controladorLocacao.Editar(id, tela.Locacao);
@@ -87,9 +87,6 @@ namespace e_Locadora.WindowsApp.Features.LocacaoModule
             if (MessageBox.Show($"Tem certeza que deseja excluir a Locação do veículo: [{locacaoSelecionado.veiculo.Modelo}] para o Cliente: [{locacaoSelecionado.cliente.Nome}]?",
                 "Exclusão de Locação", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
             {
-                //List<TaxasServicos> taxasServicosSelecionados = controladorLocacaoTaxasServicos.SelecionarTaxasServicosPorLocacaoId(locacaoSelecionado.Id);
-                //foreach (TaxasServicos taxaServicoIndividual in taxasServicosSelecionados)
-                //    controladorLocacaoTaxasServicos.ExcluirPorIdLocacaoEIdTaxa(locacaoSelecionado.Id, taxaServicoIndividual.Id);
                 if (controladorLocacao.Excluir(id))
                 {
                     tabelaLocacao.AtualizarRegistros();
