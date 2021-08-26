5901f3b8c26bccf822b00c10a7d5b4fec6361fe0
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-26 15:43:30 -0300

    Implementada Classe de Dominio Parceiro feita validação

---
+++ e-Locadora.Dominio/ParceirosModule/Parceiro.cs
@@ -0,0 +1,30 @@
+﻿using e_Locadora.Dominio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora.Dominio.ParceirosModule
+{
+    public class Parceiro : EntidadeBase
+    {
+        public string parceiro { get; }
+
+        public Parceiro(string parceiro)
+        {
+            this.parceiro = parceiro;
+        }
+
+        public override string Validar()
+        {
+            string resultadoValidacao = "";
+            if(string.IsNullOrEmpty(parceiro))
+                resultadoValidacao = "O Nome do Parceiro é obrigatório .";
+            if (resultadoValidacao == "")
+                resultadoValidacao = "ESTA_VALIDO";
+
+            return resultadoValidacao;
+        }
+    }
+}
