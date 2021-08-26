87d7503c0b8032dbcf57503678979499b4bd89ed
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-08-26 19:55:15 -0300

    Implementação das Operações e TabelaCupons

---
+++ e-Locadora.WindowsApp/Features/CuponsModule/TabelaCupons.cs
@@ -0,0 +1,66 @@
+﻿using e_Locadora.Controladores.CupomModule;
+using e_Locadora.Dominio.CupomModule;
+using e_Locadora.WindowsApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e_Locadora.WindowsApp.Features.CuponsModule
+{
+    public partial class TabelaCupons : UserControl
+    {
+        private readonly ControladorCupons  controladorCupons;
+        public TabelaCupons(ControladorCupons ctrlCupons)
+        {
+            InitializeComponent();
+            gridCupons.ConfigurarGridZebrado();
+            gridCupons.ConfigurarGridSomenteLeitura();
+            gridCupons.Columns.AddRange(ObterColunas());
+            this.controladorCupons = ctrlCupons;
+        }
+
+        private DataGridViewColumn[] ObterColunas()
+        {
+           var colunas = new DataGridViewColumn[]
+           {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Nome", HeaderText = "Nome"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "ValorPercentual", HeaderText = "Valor Percentual"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "ValorFixo", HeaderText = "Taxa Diaria"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "DataValidade", HeaderText = "Data de Validade"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Parceiro", HeaderText = "Parceiro"}
+
+           };
+
+            return colunas;
+        }
+
+        public int ObtemIdSelecionado()
+        {
+            return gridCupons.SelecionarId<int>();
+        }
+        public void AtualizarRegistros()
+        {
+
+            var cupons = controladorCupons.SelecionarTodos();
+
+            CarregarTbela(cupons);
+
+        }
+        private void CarregarTbela(List<Cupons> cupons)
+        {
+            gridCupons.DataSource = cupons;
+        }
+    }
+}
