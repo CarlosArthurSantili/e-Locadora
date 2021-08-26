e75b3a0f7e9a8a7bdf69be2573251b51f2e183c1
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-08-26 16:43:22 -0300

    Criado a class ControladorCupons

---
+++ e-Locadora.Controladores/CupomModule/ControladorCupons.cs
@@ -0,0 +1,110 @@
+﻿using e_Locadora.Controladores.Shared;
+using e_Locadora.Dominio.CupomModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora.Controladores.CupomModule
+{
+    public class ControladorCupons : Controlador<Cupons>
+    {
+        #region Queries
+
+
+        private const string sqlInserirCupom =
+        @"INSERT INTO TBCupons
+	                (	
+		                [NOME], 
+		                [VALOR_PERCENTUAL], 
+		                [VALOR_FIXO]
+	                )
+	                VALUES
+	                (
+                        @NOME, 
+		                @VALOR_PERCENTUAL, 
+		                @VALOR_FIXO
+	                )";
+
+
+        #endregion
+        public override string Editar(int id, Cupons registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string InserirNovo(Cupons registro)
+        {
+            string resultadoValidacao = registro.Validar();
+            string resultadoValidacaoControlador = ValidarCupons(registro);
+
+            if (resultadoValidacao == "ESTA_VALIDO" && resultadoValidacaoControlador == "ESTA_VALIDO")
+            {
+                registro.Id = Db.Insert(sqlInserirCupom, ObtemParametrosTaxasServicos(registro));
+            }
+
+            if (resultadoValidacao != "ESTA_VALIDO")
+            {
+                return resultadoValidacao;
+            }
+            else
+            {
+                return resultadoValidacaoControlador;
+            }
+        }
+
+        private string ValidarCupons(Cupons novoCupons, int id = 0)
+        {
+            //validar placas iguais
+            if (novoCupons != null)
+            {
+                if (id != 0)
+                {//situação de editar
+                    int countCuponsIguaiss = 0;
+                    List<Cupons> todosCupons = SelecionarTodos();
+                    foreach (Cupons cupons in todosCupons)
+                    {
+                        if (novoCupons.Nome.Equals(cupons.Nome) && cupons.Id != id)
+                            countCuponsIguaiss++;
+                    }
+                    if (countCuponsIguaiss > 0)
+                        return "Cupom já cadastrada, tente novamente.";
+                }
+                else
+                {//situação de inserir
+                    int countTaxasIguais = 0;
+                    List<Cupons> todosCupons = SelecionarTodos();
+                    foreach (Cupons cupons in todosCupons)
+                    {
+                        if (novoCupons.Nome.Equals(cupons.Nome))
+                            countTaxasIguais++;
+                    }
+                    if (countTaxasIguais > 0)
+                        return "Cupom já cadastrada, tente novamente.";
+                }
+            }
+            return "ESTA_VALIDO";
+        }
+
+        public override Cupons SelecionarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Cupons> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
