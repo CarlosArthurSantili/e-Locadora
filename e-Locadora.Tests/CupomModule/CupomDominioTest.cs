58bc73e6ef73672c3b143c4d426ce86e85a4ce96
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-08-26 15:59:30 -0300

    Criado o teste de dominio da class Cupons

---
+++ e-Locadora.Tests/CupomModule/CupomDominioTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace e_Locadora.Tests.CupomModule
+{
+    [TestClass]
+    public class CupomDominioTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
