9cf0a0a9ab72888d3abe8a7b9f4bffe1e872c514
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-26 16:49:22 -0300

    Iplimentando Fomrs

---
+++ e-Locadora.WindowsApp/Features/ParceirosModule/OperacoesParceiros.cs
@@ -0,0 +1,47 @@
+﻿using e_Locadora.WindowsApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e_Locadora.WindowsApp.Features.ParceirosModule
+{
+    public class OperacoesParceiros : ICadastravel
+    {
+
+        public void InserirNovoRegistro()
+        {
+            throw new NotImplementedException();
+        }
+        public void EditarRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirRegistro()
+        {
+            throw new NotImplementedException();
+        } 
+
+
+        public UserControl ObterTabela()
+        {
+            throw new NotImplementedException();
+        }
+        public void AgruparRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DesagruparRegistros()
+        {
+            throw new NotImplementedException();
+        }
+        public void FiltrarRegistros()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ e-Locadora.WindowsApp/Features/ParceirosModule/TabelaParceiroControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e_Locadora.WindowsApp.Features.ParceirosModule
+{
+    public partial class TabelaParceiroControl : UserControl
+    {
+        public TabelaParceiroControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
