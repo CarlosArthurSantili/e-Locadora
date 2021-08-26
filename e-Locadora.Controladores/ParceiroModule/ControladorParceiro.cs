65431a8e8a2dea17ab583fca1316a22fa9d2408c
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-26 15:54:15 -0300

    Iplmentado testes de Dominio e clase controladora pARCEIRO

---
+++ e-Locadora.Controladores/ParceiroModule/ControladorParceiro.cs
@@ -0,0 +1,44 @@
+﻿using e_Locadora.Controladores.Shared;
+using e_Locadora.Dominio.ParceirosModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora.Controladores.ParceiroModule
+{
+    public class ControladorParceiro : Controlador<Parceiro>
+    {
+
+        public override string InserirNovo(Parceiro registro)
+        {
+            throw new NotImplementedException();
+        }
+        public override string Editar(int id, Parceiro registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override Parceiro SelecionarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Parceiro> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
