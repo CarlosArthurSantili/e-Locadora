ce44cdcc3b2098c1aec77bdb4e1e0a354fef9f6e
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2021-08-26 16:54:54 -0300

    Implementado tabela control

--- e-Locadora.WindowsApp/Features/ParceirosModule/TabelaParceiroControl.cs
+++ e-Locadora.WindowsApp/Features/ParceirosModule/TabelaParceiroControl.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using e_Locadora.Controladores.ParceiroModule;
+using e_Locadora.Dominio.ParceirosModule;
+using e_Locadora.WindowsApp.Shared;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,45 @@ namespace e_Locadora.WindowsApp.Features.ParceirosModule
 {
     public partial class TabelaParceiroControl : UserControl
     {
-        public TabelaParceiroControl()
+        private readonly ControladorParceiro controlador;
+        public TabelaParceiroControl(ControladorParceiro controlador)
         {
             InitializeComponent();
+            gridParceiros.ConfigurarGridZebrado();
+            gridParceiros.ConfigurarGridSomenteLeitura();
+            gridParceiros.Columns.AddRange(ObterColunas());
+            this.controlador = controlador;
+
+        }
+
+        private DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+           {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Parceiro", HeaderText = "Nome do Parceiro"}
+           };
+
+            return colunas;
+        }
+        public int ObtemIdSelecionado()
+        {
+            return gridParceiros.SelecionarId<int>();
+        }
+
+        public void AtualizarRegistros()
+        {
+
+            var parceirosEcontrados = controlador.SelecionarTodos();
+
+            CarregarTbela(parceirosEcontrados);
+
+        }
+
+        private void CarregarTbela(List<Parceiro> parceirosEcontrados)
+        {
+            gridParceiros.DataSource = parceirosEcontrados;
         }
     }
 }
