1fc89f2c0ab8eba18cd79675f6d8dd0c9d9ae13b
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-08-30 14:53:29 -0300

    Implementação de mais testes para dominio de locação

--- e-Locadora.Tests/LocacaoModule/LocacaoDominioTests.cs
+++ e-Locadora.Tests/LocacaoModule/LocacaoDominioTests.cs
@@ -38,5 +38,69 @@ namespace e_Locadora.Tests.LocacaoModule
             //assert
             resultadoValidacao.Should().Be("ESTA_VALIDO");
         }
+
+        [TestMethod]
+        public void DeveValidar_Locacao_Caucao()
+        {
+            //arrange
+            var funcionario = new Funcionario("nome", "460162200", "usuario", "senha", DateTime.Now.Date, 600.0);
+            var grupoVeiculo = new GrupoVeiculo("Economico", 1, 2, 3, 4, 5, 6);
+            var imagem = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+            var veiculo = new Veiculo("placa", "modelo", "fabricante", 400.0, 50, 4, "123456", "azul", 4, 1996, "Grande", "Gasolina", grupoVeiculo, imagem);
+            var cliente = new Clientes("Joao", "rua souza", "9524282242", "853242", "20220220222", "1239232");
+            var condutor = new Condutor("Joao", "Rua dos Joao", "9522185224", "5222522", "20202020222", "522542", new DateTime(2022, 05, 26), cliente);
+            TaxasServicos taxaServico = new TaxasServicos("descricao", 200, 0);
+            var locacao = new Locacao(funcionario, DateTime.Now.Date, DateTime.Now.Date, 200, "Livre", 200, 0, -500, grupoVeiculo, veiculo, cliente, condutor, true);
+            
+
+            //action
+            string resultadoValidacao = locacao.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("Caução não pode ser negativo");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Locacao_SeguroCliente()
+        {
+            //arrange
+            var funcionario = new Funcionario("nome", "460162200", "usuario", "senha", DateTime.Now.Date, 600.0);
+            var grupoVeiculo = new GrupoVeiculo("Economico", 1, 2, 3, 4, 5, 6);
+            var imagem = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+            var veiculo = new Veiculo("placa", "modelo", "fabricante", 400.0, 50, 4, "123456", "azul", 4, 1996, "Grande", "Gasolina", grupoVeiculo, imagem);
+            var cliente = new Clientes("Joao", "rua souza", "9524282242", "853242", "20220220222", "1239232");
+            var condutor = new Condutor("Joao", "Rua dos Joao", "9522185224", "5222522", "20202020222", "522542", new DateTime(2022, 05, 26), cliente);
+            TaxasServicos taxaServico = new TaxasServicos("descricao", 200, 0);
+            var locacao = new Locacao(funcionario, DateTime.Now.Date, DateTime.Now.Date, 200, "Livre", -200, 0, 500, grupoVeiculo, veiculo, cliente, condutor, true);
+            
+
+
+            //action
+            string resultadoValidacao = locacao.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("Seguro do cliente não pode ser negativo");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Locacao_SeguroTerceiro()
+        {
+            //arrange
+            var funcionario = new Funcionario("nome", "460162200", "usuario", "senha", DateTime.Now.Date, 600.0);
+            var grupoVeiculo = new GrupoVeiculo("Economico", 1, 2, 3, 4, 5, 6);
+            var imagem = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+            var veiculo = new Veiculo("placa", "modelo", "fabricante", 400.0, 50, 4, "123456", "azul", 4, 1996, "Grande", "Gasolina", grupoVeiculo, imagem);
+            var cliente = new Clientes("Joao", "rua souza", "9524282242", "853242", "20220220222", "1239232");
+            var condutor = new Condutor("Joao", "Rua dos Joao", "9522185224", "5222522", "20202020222", "522542", new DateTime(2022, 05, 26), cliente);
+            TaxasServicos taxaServico = new TaxasServicos("descricao", 200, 0);
+            var locacao = new Locacao(funcionario, DateTime.Now.Date, DateTime.Now.Date, 200, "Livre", 200, -10, 500, grupoVeiculo, veiculo, cliente, condutor, true);
+
+
+            //action
+            string resultadoValidacao = locacao.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("Seguro de terceiros não pode ser negativo");
+        }
     }
 }
